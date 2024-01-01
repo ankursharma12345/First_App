544b8f0ea4812a9c6a6f7b630c6ee995187ef993
ankurs12345 <ankurs@mytaxcafe.com>
2024-01-01 15:33:16 +0530

    changes

---
+++ src/components/DispatchedComponent/ShowMessage.js
@@ -0,0 +1,70 @@
+import { Snackbar } from "@mui/material";
+import MuiAlert from "@mui/material/Alert";
+import { Fragment, forwardRef } from "react";
+import {useSelector}  from "react-redux";
+
+const Alert = forwardRef((props, ref) => {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const ShowMessage = () => {
+  const { snackbarOpen, snackbarType, snackbarMessage, snackbarTime } =
+    useSelector((state) => state.snackbar);
+
+    const handleClose = (event,reason)=>{  
+        if(reason === "clickaway") return
+    };
+
+  return (
+    <Fragment>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={snackbarOpen}
+        autoHideDuration={snackbarTime}
+        onClose={handleClose}
+        sx={{ whiteSpace: "pre-wrap" }}
+      >
+        <Alert
+          onClose={handleClose}
+          severity={snackbarType}
+          sx={
+            snackbarType === "success"
+              ? {
+                  width: "100%",
+
+                  //   backgroundColor: "#ebccd1 !important",
+                  //  color: " #a94442 !important",
+
+                  boxShadow: "none !important",
+                  marginTop: "40px !important",
+                }
+              : snackbarType === "info"
+              ? {
+                  width: "100%",
+
+                  backgroundColor: "#d1ecf1 !important",
+                  color: "#0c5460 !important",
+
+                  boxShadow: "none !important",
+                  marginTop: "40px !important",
+                }
+              : {
+                  width: "100%",
+
+                  backgroundColor: "#ebccd1 !important",
+                  color: " #a94442 !important",
+
+                  boxShadow: "none !important",
+                  marginTop: "40px !important",
+                }
+          }
+        >
+          {
+            snackbarMessage
+           }
+        </Alert>
+      </Snackbar>
+    </Fragment>
+  );
+};
+export default ShowMessage;
