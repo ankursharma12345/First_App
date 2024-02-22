54a0f90620271dcb357daad37dadedc7dd4ecd91
Ankur Sharma <100312839+ankursharma12345@users.noreply.github.com>
2024-02-22 18:22:08 +0530

    Update Login.js
    Add some more functionality and working on responsiveness

--- Login.js
+++ Login.js
@@ -1,176 +1,320 @@
-import { Alert, Box, Button, Typography } from "@mui/material";
-import React, { Fragment, useState } from "react";
-// import { ThemeProvider, createTheme } from "@mui/material/styles";
-// import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import { Link, useNavigate } from "react-router-dom";
-import Image from "../images/thomas-heintz-0tgMnMIYQ9Y-unsplash.jpg";
-// import {saveAs} from 'file-saver'
-import Snackbar from "@mui/material/Snackbar";
-import "../styles/Login.css";
-import IconButton from "@mui/material/IconButton";
-import InputAdornment from "@mui/material/InputAdornment";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { Box, Button, Grid, Typography, createTheme } from "@mui/material";
+import InputAdornment from "@mui/material/InputAdornment";
+import TextField from "@mui/material/TextField";
+import React, { Fragment, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { setSnackbar } from "../Store/Reducers/Snackbar";
+import "../styles/Login.css";
+import Banner from "../images/joanna-kosinska-llLttk4TgT4-unsplash.jpg";
+import { ThemeProvider } from "@emotion/react";
+import debounce from "lodash/debounce";
 
 const Login = () => {
   const [data, setData] = useState({});
-  // const darkTheme = createTheme({
-  //   palette: {
-  //     mode: "dark",
-  //   }
-  // });
+  console.log(data);
   const [showPassword, setShowPassword] = useState(false);
-  // const [showPassword, setShowPassword] = useState(false);
+  useEffect(() => {
+    document.getElementById("bigContainer").classList?.add("fadeInAnimation");
+  }, []);
+
+  const dispatch = useDispatch();
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
-  const [openSnackbar, setOpenSnackbar] = useState(false);
   const handleChange = (e) => {
+    // Debouncing prevent to update my state(data) on each keypress, it will automatically invoke the handleChange function when 2seconds will completed
     setData((prevData) => {
-      prevData[e.target.name] = e.target.value;
+      prevData[e?.target.id] = e?.target.value;
       return { ...prevData };
     });
   };
+  const handleBlur = (e) => {
+    setData((prev) => {
+      prev[e.target.id] = e.target.value;
+      return { ...prev };
+    });
+  };
+  // const handleBlur = (e) => {
+  //   debugger;
+  //   const { id, value } = e.target;
+
+  //   setData((prev) => {
+  //     // Update the state only if the value is not an empty string
+  //     if (value.trim() !== "") {
+  //       prev[id] = value;
+  //     }
+  //     return { ...prev };
+  //   });
+  // };
+  // const handleBlur = (e) => {
+  //   debugger;
+  //   const { id, value } = e.target;
+
+  //   setData((prev) => {
+  //     // Update the state only if the value is not an empty string
+  //     if (value.trim() !== "") {
+  //       prev[id] = value;
+  //     }
+  //     prev[id] = value;
+  //     return { ...prev };
+  //   });
+  // };
 
   const getData = JSON.parse(localStorage.getItem("Data"));
-  // const dataBlob = new Blob([JSON.stringify(getData)]);
-  // saveAs(dataBlob, 'server1.json');
-
-  // const getDataItem = {...getData}
-
   const getName = getData?.Name;
-  // console.log(getName);
-
   const getPassword = getData?.password;
-  // console.log(getPassword);
 
   const navigate = useNavigate();
 
   const handleClick = () => {
-    if (getName === data.Username && getPassword === data.password) {
-      setOpenSnackbar(true);
-      setTimeout(() => {
-        navigate("/home"); // We can also use navigate with callback functions but we don't have to use Link with callback functions
-      }, 400);
+    if (getName) {
+      if (getName === data.Username && getPassword === data.password) {
+        dispatch(
+          setSnackbar(
+            true,
+            "success",
+            `${"Login Successfully!!"}${" " + getName}`
+          )
+        );
+        setTimeout(() => {
+          navigate("/home"); // We can also use navigate with callback functions but we don't have to use Link with callback functions
+        }, 400);
+        return;
+      }
     }
+    if (!data?.Username || !data?.password) {
+      return dispatch(
+        setSnackbar(true, "error", "Please! Fill your details first")
+      );
+    }
+    if (getName !== data.Username || getPassword !== data.password) {
+      debugger;
+      // dispatch(setLoader("start"));
+      dispatch(setSnackbar(true, "error", "Please! check your credentials"));
+      const getData = document.getElementById("Username");
+      getData?.focus();
+      // Use the next event loop cycle to focus after updating the state
+      setTimeout(() => {
+        setData((prevData) => {
+          return { ...prevData, Username: "", password: "" };
+        });
+      }, 10);
+
+      return;
+    }
+    // if (getName !== data.Username || getPassword !== data.password) {
+    //   debugger;
+    //   // dispatch(setLoader("start"));
+    //   dispatch(setSnackbar(true, "error", "Please! check your credentials"));
+    //   const getData = document.getElementById("Username");
+    //   setTimeout(() => {
+    //     getData?.focus();
+    //   }, 10);
+    //     setData((prevData) => {
+    //       return { ...prevData, Username: "", password: "" };
+    //     });
+    //   // setTimeout(() => {
+    //   //   dispatch(setLoader("stop"));
+    //   // }, 500);
+    //   // setData((prevData) => {
+    //   //   return { ...prevData, Username: "", password: "" };
+    //   // });
+    //   return;
+    // }
   };
   const handleRegister = () => {
     setTimeout(() => {
       navigate("/register", { replace: true });
     }, 400);
   };
+  const theme = createTheme({
+    components: {
+      MuiInputLabel: {
+        styleOverrides: {
+          root: {
+            fontSize: "1.3rem",
+            color: "yellow",
+          },
+        },
+      },
+    },
+  });
 
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-    setOpenSnackbar(false);
-  };
   return (
     <Fragment>
-      {/* <ThemeProvider theme={darkTheme}> */}
-      <Box
+      <Grid
+        container
+        className="container"
+        id="bigContainer"
+        height="100vh"
+        minWidth="100vw"
         sx={{
-          backgroundImage: `url(${Image})`,
-          height: "100vh",
-          backgroundPositionY: "fixed",
+          backgroundImage: `url(${Banner})`,
+          backgroundSize: "contained",
+          backgroundRepeat: "no-repeat",
+          overflowX: "hidden",
+          overflowY: "hidden",
+          // flexWrap: "wrap",
         }}
       >
-        {" "}
-        {/* For fixing the scroll we use backgroundPositionY: 'fixed' */}
-        {/* <CssBaseline /> */}
-        <Typography
-          sx={{ color: "green", fontSize: 30, ml: "42.5%", fontWeight: "bold" }}
-        >
-          Login Here!!
-        </Typography>
-        {/* <Box sx={{ backgroundColor: "goldenrod", height: "5rem" }}>
-          <Typography
-            sx={{
-              color: "white",
-              ml: "570px",
-              fontSize: "30px",
-              mt: "30px",
-            }}
-          >
-            Login First!
-          </Typography>
-        </Box> */}
-        <Box
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={12}
+          // direction="column"
           sx={{
-            width: 400,
-            height: 410,
-            backgroundColor: "grey",
+            display: "flex",
+            alignItems: "center",
             justifyContent: "center",
-            marginLeft: "34%",
-            mt: "55px",
-            // mb: "40px",
           }}
         >
-          <TextField
-            autoFocus="true"
-            label="Username"
-            variant="standard"
-            name="Username"
-            color="success"
-            value={data.name}
-            autoComplete="off" // It will not shown any hint
-            onChange={handleChange}
-            sx={{ justifyContent: "center", ml: 10, mt: 9 }}
-          ></TextField>
-          <br />
-          <br />
-          <TextField
-            label="Password"
-            variant="standard"
-            name="password"
-            value={data.name}
-            type={showPassword ? "text" : "password"}
-            onChange={handleChange}
-            sx={{ justifyContent: "center", ml: 10 }}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton onClick={handleClickShowPassword} edge="end">
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-          ></TextField>
-          <Button
-            variant="contained"
-            sx={{ mt: 6, mr: 7, ml: 9.5 }}
-            onClick={handleClick}
+          <Box
+            className="box"
+            sx={{
+              width: { xs: 280, sm: 300, md: 400 },
+              height: { xs: 340, sm: 380, md: 450 },
+              // backgroundColor: "#598dc2",
+              backgroundColor: "transparent",
+              boxShadow: "rgba(0.3,0.3,0.3,0.3)",
+              margin: "auto", // For centering the Box horizontally
+              // mt: { xs: -12, sm: -12, md: -3 },
+            }}
           >
-            Sign in
-          </Button>
-          <Snackbar
-            className="snack"
-            open={openSnackbar}
-            autoHideDuration={3000}
-            onClose={handleClose}
-            // sx={{backgroundColor:'green'}}
-          >
-            <Alert
-              onClose={handleClose}
-              severity="success"
-              sx={{ width: "100%", backgroundColor: "green", color: "white" }}
+            <Grid
+              item
+              xs={12}
+              sm={12}
+              md={12}
+              alignItems="center"
+              justifyContent="center"
+              display="flex"
+              marginTop="1rem"
+              // marginTop="auto"
             >
-              Logged In!!
-            </Alert>
-          </Snackbar>
-          <Button
-            variant="contained"
-            sx={{ mr: 5, mt: -7.1, ml: 24.5 }}
-            onClick={handleRegister}
-          >
-            Register
-          </Button>
-          <Typography sx={{ ml: 9, color: "white", mt: 1 }}>
-            New User? <Link to={"/register"}>Register</Link> yourself first !!
-          </Typography>
-        </Box>
-        {/* </ThemeProvider> */}
-      </Box>
+              <Typography variant="h4">Sign In</Typography>
+            </Grid>
+            <Grid item xs={10} sm={12} md={12}>
+              {" "}
+              <ThemeProvider theme={theme}>
+                <TextField
+                  className="textField1"
+                  autoFocus={true} // autoFocus should be written like this else it throw error. we don't pass like autoFocus="true"
+                  required={true} // required should be written like this else it throw error.we don't pass like required="true"
+                  // color="secondary"
+                  label="Username"
+                  variant="standard"
+                  id="Username"
+                  // color="success"
+                  // value={data?.Username}
+                  value={data?.["Username"]}
+                  autoComplete="off" // It will not show any hint
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  sx={{
+                    justifyContent: "center",
+                    ml: { xs: 5, sm: 4, md: 11 },
+                    mt: { xs: 2, sm: 4, md: 4 },
+                  }}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <AccountCircleIcon fontSize="medium" />
+                      </InputAdornment>
+                    ),
+                  }}
+                ></TextField>
+              </ThemeProvider>
+            </Grid>
+            <Grid item xs={10} sm={12} md={12}>
+              {" "}
+              <ThemeProvider theme={theme}>
+                <TextField
+                  className="textField2"
+                  label="Password"
+                  required={true}
+                  variant="standard"
+                  id="password"
+                  value={data?.["password"]}
+                  type={showPassword ? "password" : "text"}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  onClick={handleClickShowPassword}
+                  sx={{
+                    justifyContent: "center",
+                    ml: { xs: 5, sm: 4, md: 11 },
+                    mt: { xs: 2, sm: 4, md: 4 },
+                  }}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </InputAdornment>
+                    ),
+                  }}
+                ></TextField>
+              </ThemeProvider>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12}>
+              {" "}
+              <Button
+                variant="contained"
+                sx={{
+                  mt: { xs: 5, sm: 4, md: 6 },
+                  ml: { xs: 5, sm: 3, md: 10 },
+                }}
+                onClick={handleClick}
+              >
+                Sign in
+              </Button>
+              <Button
+                variant="contained"
+                sx={{
+                  mt: { xs: 5, sm: 4, md: -7 },
+                  ml: { xs: 3, sm: 4, md: 24.5 },
+                }}
+                onClick={handleRegister}
+              >
+                Register
+              </Button>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              sm={12}
+              md={12}
+              justifyContent="center"
+              alignItems="center"
+              display="flex"
+            >
+              {" "}
+              <Typography
+                sx={{
+                  // justifyContent: "center",
+                  // alignItems: "center",
+                  // display: "flex",
+                  // ml: { xs: 2, sm: 2, md: 6.4 },
+                  mt: { xs: 2, sm: 2, md: 1 },
+                  color: "black",
+                  fontWeight: "bold",
+                  fontStyle: "italic",
+                  // fontSize: "19px",
+                }}
+              >
+                New User?{" "}
+                <b>
+                  <Link to={"/register"}> Register </Link>
+                </b>{" "}
+                yourself first !!
+              </Typography>
+            </Grid>
+          </Box>
+        </Grid>
+      </Grid>
+      {/* </video> */}
     </Fragment>
   );
 };
